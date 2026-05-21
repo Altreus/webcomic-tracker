85191af58843558fdf4413b2a98063de1f171374
Alastair Douglas <altreus@altre.us>
2026-05-21 15:32:41 +0100

    Group by date of latest entry
    And show last read date

--- webcomic-reminder.pl
+++ webcomic-reminder.pl
@@ -1,15 +1,28 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite -signatures;
 use Mojo::Feed;
+# Can't really render a Mojo::Date in the way I want, but DateTime is a
+# bugger to parse a date into, so using both together seems fairly smooth
 use Mojo::Date;
+use DateTime;
 use Mojo::SQLite;
 
 my $sqlite = Mojo::SQLite->new('comics.db');
 
+helper truncate_to_date => sub($c, $d) {
+    my $dt = DateTime->from_epoch( epoch => Mojo::Date->new($d)->epoch );
+    $dt->truncate( to => 'day' );
+    $dt->rfc3339
+};
+
+helper local_date => sub($c, $d) {
+    DateTime->from_epoch( epoch => Mojo::Date->new($d)->epoch )->strftime('%x') 
+};
+
 get '/' => sub ($c) {
     my $comics = $sqlite->db->query('SELECT * FROM comic');
 
-    my @comics;
+    my %dates;
     while (my $comic = $comics->hash) {
         my $feed = Mojo::Feed->new( url => $comic->{feed_url} );
         $feed->entries->each(sub {
@@ -18,31 +31,20 @@ get '/' => sub ($c) {
             );
         });
 
-        push @comics, { name => $comic->{name} };
+        # Don't break if for some reason no entries get added from the feed
+        my $latest_entry = latest_entry_for($comic->{name}) or next;
+        my $last_read    = last_read_for($comic->{name});
+
+        push $dates{$c->truncate_to_date( $latest_entry->{pubdate} )}->{comics}->@*,
+            { name => $comic->{name}, last_read => $last_read->{pubdate} };
     }
-    $c->stash( comics => \@comics );
+
+    $c->stash( dates => \%dates );
     $c->render(template => 'index');
 };
 
 get '/goto' => sub ($c) {
-    my $q = $sqlite->db->query(<<~'SQL', $c->param('comic'));
-        SELECT * FROM url
-        WHERE comic_name = ?
-        AND opened = 1
-        ORDER BY datetime(pubdate) DESC
-        LIMIT 1
-        SQL
-    my $latest_entry = $q->hash;
-
-    unless ($latest_entry) {
-        # If no latest entry, none has been opened, so get the first one by date
-        my $q = $sqlite->db->query(<<~'SQL', $c->param('comic'));
-            SELECT * FROM url
-            WHERE comic_name = ?
-            ORDER BY datetime(pubdate) ASC LIMIT 1
-            SQL
-        $latest_entry = $q->hash;
-    }
+    my $last_read = last_read_for($c->param('comic'));
 
     # Set everything currently in the DB as opened.
     $sqlite->db->query(<<~'SQL', $c->param('comic'));
@@ -50,8 +52,43 @@ get '/goto' => sub ($c) {
         SQL
 
     # Redirect to the last-opened URL
-    $c->redirect_to($latest_entry->{url});
+    $c->redirect_to($last_read->{url});
 };
+
+sub latest_entry_for($comic_name) {
+    my $q = $sqlite->db->query(<<~'SQL', $comic_name);
+        SELECT * FROM url
+        WHERE comic_name = ?
+        ORDER BY datetime(pubdate) DESC
+        LIMIT 1
+        SQL
+    my $latest_entry = $q->hash;
+
+    return $latest_entry;
+}
+
+sub last_read_for($comic_name) {
+    my $q = $sqlite->db->query(<<~'SQL', $comic_name);
+        SELECT * FROM url
+        WHERE comic_name = ?
+        AND opened = 1
+        ORDER BY datetime(pubdate) DESC
+        LIMIT 1
+        SQL
+    my $last_read = $q->hash;
+
+    unless ($last_read) {
+        # If no latest entry, none has been opened, so get the first one by date
+        my $q = $sqlite->db->query(<<~'SQL', $comic_name);
+            SELECT * FROM url
+            WHERE comic_name = ?
+            ORDER BY datetime(pubdate) ASC LIMIT 1
+            SQL
+        $last_read = $q->hash;
+    }
+
+    return $last_read;
+}
 
 app->start;
 __DATA__
@@ -59,9 +96,13 @@ __DATA__
 @@ index.html.ep
 % layout 'default';
 % title 'Welcome';
-<h1>Welcome to the Mojolicious real-time web framework!</h1>
-<% for my $c (@$comics) { %>
-    <p><a href="/goto?comic=<%= $c->{name} %>"><%= $c->{name} %></a></p>
+<% for my $d (reverse sort keys %$dates) { %>
+    <h2><%= $c->local_date( $d ) %></h2>
+    <% for my $comic ($dates->{$d}->{comics}->@*) { %>
+        <p><a href="/goto?comic=<%= $comic->{name} %>" target="_blank">
+            <%= $comic->{name} %></a> Last read: <%= $c->local_date( $comic->{last_read} ) %>
+        </p>
+    <% } %>
 <% } %>
 
 @@ layouts/default.html.ep
